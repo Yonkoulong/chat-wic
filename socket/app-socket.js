5ecb00f4e24452ea6ce4c9069b832c1b8017180e
LongNH <phongdt@smartosc.com>
2023-02-14 16:40:31 +0700

    feat : on and emit socket.io

--- socket/app-socket.js
+++ socket/app-socket.js
@@ -1,4 +1,5 @@
 const socketIO = require("socket.io");
+let usersActive = [];
 
 const setUpSocketIO = (http) => {
   const socketConfig = socketIO(http, {
@@ -6,10 +7,28 @@ const setUpSocketIO = (http) => {
       origin: process?.env?.PORT_CLIENT || "http://localhost:5173",
     },
   });
+
   socketConfig.on("connection", (socket) => {
-    console.log(`Connect successfully ${socket.id}`);
+    console.log(socket);
+    // sending between react app and socket server
+    socket.on("message", (data) => {
+      socketConfig.emit("messageResponse", data);
+    });
+
+    // fetch active users from socket.io
+    socket.on("newUser", (data) => {
+      usersActive.push(data);
+      socketConfig.emit("newUserResponse", usersActive);
+    });
+
+    // detect user typing
+    socket.on("typing", (data) => socketConfig.emit("typingResponse", data));
+
+    // user leave app disconnect and update
     socket.on("disconnect", () => {
-      console.log("Socket disconnect");
+      usersActive = usersActive.filter((user) => user?.socketId !== socket.id);
+      socketConfig.emit("newUserResponse", usersActive);
+      socket.disconnect();
     });
   });
 };
