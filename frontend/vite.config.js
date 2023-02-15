898ad9ca6f4ea7f8977427d7b4c0f3a70444b7ea
LongNH <phongdt@smartosc.com>
2023-02-15 10:06:10 +0700

    config : host front end

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite'
-import * as path from 'path'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import * as path from "path";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, './src') }],
-    extensions: ['.jsx',['.js']]
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "./src") }],
+    extensions: [".jsx", [".js"]],
   },
-})
+  server: {
+    port: 3000,
+  },
+});
