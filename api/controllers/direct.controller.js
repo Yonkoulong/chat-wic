a56cda595e2fdb2454d923bffe7cd227a363b1ee
longNH <nguyenhailong270921@gmail.com>
2023-03-24 14:04:30 +0700

    feat : get direct by user id

--- api/controllers/direct.controller.js
+++ api/controllers/direct.controller.js
@@ -1,4 +1,5 @@
 const DirectModel = require("../models/direct.model");
+const UserModel = require("../models/user.model");
 const {
   httpCode,
   IUserStatus,
@@ -21,32 +22,57 @@ const {
 const bcrypt = require("bcrypt");
 
 const postCreateDirect = async (req, res) => {
-  const { messageFrom } = req?.body;
+  const { userIds, organizeId } = req?.body;
 
-  if (!isArray(userIds && isObjectIdInMongodb(ownerId))) {
-    return res.status(httpCode.badRequest).json(responseError.badRequest);
+  if(!userIds || !organizeId || !isObjectIdInMongodb(organizeId)){
+    return res?.status(httpCode.badRequest).json(responseError.badRequest); 
   }
 
-  const newChannel = {
-    channelName,
-    userIds,
-    ownerId,
-  };
-  try {
-    await DirectModel?.create(newChannel);
-    return res?.status(httpCode.ok).json({ content: newChannel });
-  } catch {
-    return res?.status(httpCode.badRequest).json(responseError.badRequest);
+  const convertUserIds = isArray(userIds) ? userIds : [];
+  const directId = new ObjectIdMongodb();
+  const newDirect = {
+    _id : directId,
+    userIds : convertUserIds,
+    organizeId
   }
+
+  try { 
+    await DirectModel.create(newDirect);
+    return res.status(httpCode.ok).json(formatResponse(newDirect)); 
+  }catch{
+    return res?.status(httpCode.badRequest).json(responseError.badRequest); 
+  }
+
+  
 };
 
 const postGetDirectsByUserId = async (req, res) => {
-  const { userId } = req?.params;
+  const { userId , organizeId} = req?.params;
   try {
-    const channelsByUserId = await DirectModel.find({
-      userIds: { $in: [userId] },
+    let directByUserId = await DirectModel.find({
+      userIds: { $in: [userId] },organizeId
     });
-    return res.status(httpCode.ok).json(formatResponse(channelsByUserId));
+
+    let usersIdsInDirects = [];
+
+    directByUserId?._doc?.forEach(item => {
+      const ids = isArray(item?.userIds) ? item.userIds : [];
+      usersIdsInDirects = [...usersIdsInDirects, ...ids] ;
+    });
+
+    const usersInfoInDirects = await UserModel.find({id : {$in : usersIdsInDirects}});
+    directByUserId.forEach((item, index) => {
+      const ids = isArray(item?.userIds) ? item.userIds : [];
+      let usersInfo = [];
+      usersInfoInDirects.forEach(user => {
+        if(ids.includes(user?.id)){
+          usersInfo.push(user)
+        }
+      })
+      directByUserId[index].usersInfo = usersInfo;
+    })
+
+    return res.status(httpCode.ok).json(formatResponse(directByUserId));
   } catch {
     return res?.status(httpCode.badRequest).json(responseError.badRequest);
   }
