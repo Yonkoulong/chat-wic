999200d01d1de2df5519a223a6c1532ab777448d
longnh <longnh@smartosc.com>
2023-04-17 16:31:22 +0700

    Feat: change API URL

--- frontend/src/pages/ChatView/index.jsx
+++ frontend/src/pages/ChatView/index.jsx
@@ -1,15 +1,14 @@
-import React, { useEffect } from "react";
-import { Outlet } from "react-router-dom";
-import Sidebar from "./Sidebar";
-import { ChatViewContainer } from "./ChatView.styles";
+import React, { useEffect } from 'react';
+import { Outlet } from 'react-router-dom';
+import Sidebar from './Sidebar';
+import { ChatViewContainer } from './ChatView.styles';
 
-import io from "socket.io-client";
-import { useSocketStore } from "@/stores/SocketStore";
-import { useAppStore } from "@/stores/AppStore";
-import { getChannelsByUser } from "@/services/channel.services";
-import { getDirectsByUserId } from "@/services/direct.services";
-import { useRoomStore } from "@/stores/RoomStore";
-
+import io from 'socket.io-client';
+import { useSocketStore } from '@/stores/SocketStore';
+import { useAppStore } from '@/stores/AppStore';
+import { getChannelsByUser } from '@/services/channel.services';
+import { getDirectsByUserId } from '@/services/direct.services';
+import { useRoomStore } from '@/stores/RoomStore';
 
 export const ChatView = () => {
   const { setClient, client } = useSocketStore((state) => state);
@@ -17,14 +16,19 @@ export const ChatView = () => {
   const { setChannelRooms, setDirectRooms } = useRoomStore((state) => state);
 
   useEffect(() => {
-    const client = io("http://localhost:8080", {
-      extraHeaders: { userId: userInfo?._id },
-    });
-    client.on("connect", () => {
+    const client = io(
+      import.meta.env.VITE_CHAT_WIC_API_ENV == 'dev'
+        ? import.meta.env.VITE_CHAT_WIC_API || 'http://localhost:8080'
+        : 'https://be-chat-wic.onrender.com',
+      {
+        extraHeaders: { userId: userInfo?._id },
+      }
+    );
+    client.on('connect', () => {
       setClient(client);
 
       //channel
-      client.on("invited-to-a-channel", async () => {
+      client.on('invited-to-a-channel', async () => {
         const respChannels = await getChannelsByUser(userInfo);
         if (Array.isArray(respChannels?.data?.content)) {
           setChannelRooms(respChannels?.data?.content);
@@ -32,15 +36,15 @@ export const ChatView = () => {
       });
 
       //direct
-      client.on("invited-to-a-direct", async () => {
+      client.on('invited-to-a-direct', async () => {
         const respDirects = await getDirectsByUserId(userInfo);
-        if(Array.isArray(respDirects?.data?.content)) {
+        if (Array.isArray(respDirects?.data?.content)) {
           setDirectRooms(respDirects?.data?.content);
         }
-      })
+      });
     });
 
-    client.on("disconnect", () => {
+    client.on('disconnect', () => {
       setClient(null);
     });
 
